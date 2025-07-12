a0b8bc2405922e0d9f41c365eb22b3e8cdce4eaa
Md. Sakib Mia <sakibatreus@gmail.com>
2025-07-12 18:07:45 +0600

    update service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hey...! Welcome to the Movie Server!!!';
+    return 'Hey..! Welcome to the Movie Server!!!';
   }
 }
